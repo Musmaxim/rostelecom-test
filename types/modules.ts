1a8cb65e0eeeaecac3d49afbb0852bcaa5bd90db
Mus Maxim <musmaxim.musienko@yandex.ru>
2024-02-24 17:02:39 +0300

    Создаем наполнение меню с каталогаом и одеждой

---
+++ types/modules.ts
@@ -0,0 +1,15 @@
+export interface IAcccordionProps {
+  children: React.ReactNode;
+  title: string | JSX.Element;
+  titleClass: string;
+  rotateIconClass?: string;
+}
+
+export interface IMenuLinkItemProps {
+  item: {
+    id: number;
+    text: string;
+    href: string;
+  };
+  handleRedirectToCatalog: (arg0: string) => void;
+}
