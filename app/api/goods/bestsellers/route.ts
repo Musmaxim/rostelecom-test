efbb3e4ace6f65f5b26b9520eb7173ecb318994f
Mus Maxim <musmaxim.musienko@yandex.ru>
2024-03-13 21:23:59 +0300

    Подкючили Mongo, создали роуты одежды и аксессуары

---
+++ app/api/goods/bestsellers/route.ts
@@ -0,0 +1,12 @@
+import clientPromise from "@/lib/mongodb";
+import {
+  getDbAndReqBody,
+  getNewAndBestsellersGoods,
+} from "@/lib/utils/api-routes";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const { db } = await getDbAndReqBody(clientPromise, null);
+
+  return NextResponse.json(await getNewAndBestsellersGoods(db, "isBestseller"));
+}
